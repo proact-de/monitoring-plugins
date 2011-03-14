f232450fec6d51492828a7f35e66a8540f6340d4
Sven Velt <sven@velt.de>
2011-03-14 19:16:30 +0100

    Fix template for host overview - WIP
    Signed-off-by: Sven Velt <sven@velt.de>

--- PNP/templates/check_naf.php
+++ PNP/templates/check_naf.php
@@ -7,30 +7,7 @@
 ### Helper functions
 #
 
-function wc($VAL) {
-	$wcout = "";
-
-	if ($VAL['WARN'] != "") {
-		$wcout .= rrd::hrule($VAL['WARN'], "#FFFF00", "Warning  " . $VAL['WARN'] . "\\n");
-	}
-	if ($VAL['WARN_MIN'] != "") {
-		$wcout .= rrd::hrule($VAL['WARN_MIN'], "#FFFF00", "Warning  (min)  " . $VAL['WARN_MIN'] . "\\n");
-	}
-	if ($VAL['WARN_MAX'] != "") {
-		$wcout .= rrd::hrule($VAL['WARN_MAX'], "#FFFF00", "Warning  (max)  " . $VAL['WARN_MAX'] . "\\n");
-	}
-	if ($VAL['CRIT'] != "") {
-		$wcout .= rrd::hrule($VAL['CRIT'], "#FF0000", "Critical " . $VAL['CRIT'] . "\\n");
-	}
-	if ($VAL['CRIT_MIN'] != "") {
-		$wcout .= rrd::hrule($VAL['CRIT_MIN'], "#FF0000", "Critical (min)  " . $VAL['CRIT_MIN'] . "\\n");
-	}
-	if ($VAL['CRIT_MAX'] != "") {
-		$wcout .= rrd::hrule($VAL['CRIT_MAX'], "#FF0000", "Critical (max)  " . $VAL['CRIT_MAX'] . "\\n");
-	}
-
-	return $wcout;
-}
+require_once("check_naf_helper.php");
 
 #
 ### Main loop
